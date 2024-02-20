facbd9559574f7d03ef12696f84727bf905deea3
QusaiAlnjem <155255033+QusaiAlnjem@users.noreply.github.com>
2024-02-20 21:27:51 +0400

    Add files via upload

---
+++ first_lecture.py
@@ -0,0 +1,69 @@
+""" DataTypes """
+# Text Type:	str
+# Numeric Types:	int, float
+# data structure Types:   list, tuple, set, dict
+# Boolean Type:	bool
+# None Type:	NoneType
+
+""" Comment """
+print("my name is qusai") # print the sentence
+"""this comment is
+    too long """
+"this is also a comment"
+
+""" Print """
+print("Hello World")
+print(10 , "Eren Yeager")
+
+""" Variables """
+num = 5
+print("number is" , num)
+
+name = "Daniel"
+age = 20
+print("My name is" , name , "and I am", age , "Years old")
+name = "Qusai"
+age = 30
+print("My name is" , name , "and I am", age , "Years old")
+
+""" Type Conversion """
+var1 = 10
+print(var1)
+print(type(var1))
+
+var1 = str(var1)
+print(var1)
+print(type(var1))
+
+var1 = float(var1)
+print(var1)
+print(type(var1))
+
+var1 = bool(var1)
+print(var1)
+print(type(var1))
+
+""" Arithmetic Operations """
+# +	Addition	x + y	
+# -	Subtraction	x - y	
+# *	Multiplication	x * y	
+# /	Division	x / y	
+# %	Modulus	x % y	
+# **	Exponentiation	x ** y	
+# //	Floor division	x // y
+x = 20
+y = 3
+print("+	Addition:" , x + y)	
+print("-	Subtraction:" , x - y)
+print("*	Multiplication:" , x * y)
+print("/	Division:" , x / y)
+print("%	Modulus:" , x % y)
+print("**	Exponentiation:" , x ** y)	
+print("//	Floor division:" , x // y)
+
+""" User Input """
+email = input("Enter Your Email Please: ")
+password = input("Enter Your Password: ")
+
+print("Your email: " , email)
+print("Your password: " , password)
